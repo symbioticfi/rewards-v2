1fb62b69db575967ad06f2ed5e74c7f73acbb6f0
Sergii Liakh <liakh@ukr.net>
2025-10-24 11:23:41 +0100

    chore: added SymbioticCoreConstants to deploy script

--- script/deploy/Rewards.s.sol
+++ script/deploy/Rewards.s.sol
@@ -7,26 +7,26 @@ import {Rewards} from "../../src/contracts/Rewards.sol";
 import {IRewards} from "../../src/interfaces/IRewards.sol";
 import {IVaultSnapshotRewards} from "../../src/interfaces/IVaultSnapshotRewards.sol";
 
+import {SymbioticCoreConstants} from "@symbioticfi/core/test/integration/SymbioticCoreConstants.sol";
+
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract RewardsScript is Script {
-    // Configuration constants - UPDATE THESE BEFORE EXECUTING
-
-    // Address of the VaultFactory contract
-    address public constant VAULT_FACTORY = address(0x1);
-    // Address of the NetworkRegistry contract
-    address public constant NETWORK_REGISTRY = address(0x2);
-    // Address of the NetworkMiddlewareService contract
-    address public constant NETWORK_MIDDLEWARE_SERVICE = address(0x3);
-
     function run(address feeRegistry, address curatorRegistry, address owner, address admin)
         external
         returns (address)
     {
+        SymbioticCoreConstants.Core memory core = SymbioticCoreConstants.core();
+
         vm.startBroadcast();
 
-        Rewards implementation =
-            new Rewards(VAULT_FACTORY, NETWORK_REGISTRY, NETWORK_MIDDLEWARE_SERVICE, curatorRegistry, feeRegistry);
+        Rewards implementation = new Rewards(
+            address(core.vaultFactory),
+            address(core.networkRegistry),
+            address(core.networkMiddlewareService),
+            curatorRegistry,
+            feeRegistry
+        );
         bytes memory initData = abi.encodeWithSelector(Rewards.initialize.selector, owner);
         TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(address(implementation), admin, initData);
 
